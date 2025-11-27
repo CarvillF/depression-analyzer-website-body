68810701260bb4c37ea7a3acc685f0424b950356
gracee <gracee@vt.edu>
2025-11-26 21:34:43 -0500

    Addedl random forest code

---
+++ ml_grace/configs.py
@@ -0,0 +1,22 @@
+'''
+Contains configuration information for easy access and updating
+'''
+
+import pandas as pd
+
+data_dir = '/home/ugrads/majors/gracee/CS2104/depression-analyzer-website-body/ml_grace/Depression Student Dataset.csv'
+test_ratio = 0.3
+num_trees = 10000
+
+sample = pd.DataFrame([{
+    'Gender': 1,
+    'Age': 19,
+    'Academic Pressure': 2.0,
+    'Study Satisfaction': 3.0,
+    'Sleep Duration': 4,
+    'Dietary Habits': 3,
+    'Have you ever had suicidal thoughts ?': 0,
+    'Study Hours': 8,
+    'Financial Stress': 1,
+    'Family History of Mental Illness': 0
+}])
